8b49273d93638629b3b5a258bb4a8d3f2baa7e55
daydocx <contato.dayseguedes@gmail.com>
2023-08-14 11:35:54 -0300

    orientacao a objetos

---
+++ telasom/Programa.cs
@@ -0,0 +1,15 @@
+﻿Musica musica1 = new Musica();
+musica1.nome = "Divino Maravilhoso";
+musica1.artista = "Gal Costa";
+musica1.duracao = 273;
+musica1.disponivel = true;
+
+Musica musica2 = new Musica();
+musica2.nome = "Azul";
+musica2.artista = "Chico Buarque";
+musica2.duracao = 373;
+musica2.disponivel = false;
+
+musica1.ExibirFichaTecnica();
+
+musica2.ExibirFichaTecnica();
